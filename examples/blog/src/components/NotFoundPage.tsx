d157608110d8acafb1705eea26b8bc2ac01530f1
srdjan <srdjan@timok.com>
2025-06-14 09:00:08 -0400

    removed examples

--- examples/blog/src/components/NotFoundPage.tsx
+++
@@ -1,9 +0,0 @@
-export function NotFoundPage() {
-  return (
-    <>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you're looking for doesn't exist.</p>
-      <a href="/">← Back to Home</a>
-    </>
-  );
-}
